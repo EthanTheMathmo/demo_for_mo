5d12ed6dd94615105e560601cc3ef0b2f6b5948d
EthanTheMathmo <eh595@cam.ac.uk>
2021-08-13 19:17:14 +0100

    error handling for highlight dates to expiry

--- for_mo.py
+++ for_mo.py
@@ -1,6 +1,6 @@
 import xlwings as xw
 from index_helpers import block_to_list
-from test_gui import file_address_gui
+from test_gui import file_address_gui, simple_error
 import datetime
 import re
 
@@ -63,46 +63,43 @@ def color_cells(color_dict, wb):
             pass
     return
 
-def highlight_dates_to_expiry():
+def helper_date_to_expiry(values, address_as_list, purple_list, red_list, yellow_list, green_list):
     """
-    Given a selection of cells which are dates
-    TO-DO:
-        ERROR HANDLING FOR IF THE VALUES ARE NOT DATETIMES
-        Change the color coding based on datetimes to ones requested. (Currently: 1 year green, 6mths-1yr yellow, less than 
-        6 months is red. Should probably be more of a gradation, perhaps with red set aside for if it has expired?)
+    Helper code for highlight_dates_to_expiry, for code which is needed in both the case where the user selects
+    multiple blocks, and the case where the user selects only one block
+    """    
     
-    """
-    wb = xw.apps.active.books.active #high cost
-    user_selection = wb.selection #high cost cost
-    values = user_selection.value #low cost
     if type(values) != list:
-        #e.g., if we select a single cell, this puts it in an array, so that the code runs in the same way 
-        #for multiple values and for single values
+    #e.g., if we select a single cell, this puts it in an array, so that the code runs in the same way 
+    #for multiple values and for single values
         values = [values]
     else:
         pass
     
-    address = user_selection.address #low cost
-    address_as_list = block_to_list(address).split(",")
-
-    green_list = []
-    yellow_list = []
-    red_list = []
-    purple_list = [] #for cells whose values we couldn't read as a date
-
+    if type(values[0]) == list:
+        #if it is an array then we raise an error message, as this is meant to act on columns and single cells
+        #the reason for this is twofold: (1) from the sheets shown, it seems if an array is selected it's not actually likely
+        #to be helpful for this function and (2) it makes P(bug) higher in my experience, so if it is useful/requested
+        #we should do it, but not pre-emptively
+        simple_error(error_title="A minor hiccup...",error_body="Oops - please only select columns or individual cells")
+        return
+    else:
+        pass
+    
     for i, val in enumerate(values):
         if val == None:
             continue
         else:
             pass
-        
+
         if type(val) != datetime.datetime:
             #tries to convert to a datetime if the input formula is a bit off e.g., 18/9/18 instead of 18/9/2018
-            a = re.match(r"([0-2]{0,1}[0-9]||30||31)[/.]([0]{0,1}[1-9]||[1][0-2])[/.][0-9][0-9]", val)
+            #str(val) is because regex requires a string to be passed in
+            a = re.match(r"([0-2]{0,1}[0-9]||30||31)[/.]([0]{0,1}[1-9]||[1][0-2])[/.][0-9][0-9]", str(val))
             if a:
                 #we matched the regex
                 try:
-                    #try to make a datetime
+                    #try to make a datetime, e.g. for 12.05.20 
                     a = re.split("[.]|/",a.group())
                     val = datetime.datetime(day=a[0], month=a[1], year=int("20"+a[2]))
                 except:
@@ -128,7 +125,49 @@ def highlight_dates_to_expiry():
         else:
             pass
 
+
+def highlight_dates_to_expiry():
+    """
+    Given a selection of cells which are dates
+    TO-DO:
+        ERROR HANDLING FOR IF THE VALUES ARE NOT DATETIMES
+        Change the color coding based on datetimes to ones requested. (Currently: 1 year green, 6mths-1yr yellow, less than 
+        6 months is red. Should probably be more of a gradation, perhaps with red set aside for if it has expired?)
     
+    """
+    wb = xw.apps.active.books.active #high cost
+    user_selection = wb.selection #high cost cost
+    green_list = []
+    yellow_list = []
+    red_list = []
+    purple_list = [] #for cells whose values we couldn't read as a date
+
+
+
+    address = user_selection.address #low cost
+
+    if "," in address:
+        #means multiple columns have been passed in, and the handling is slower, and more complicated
+        address_columns = address.split(",")
+        for block in address_columns:
+            address_as_list = block_to_list(block).split(",")
+            values = wb.sheets.active.range(block).value
+            helper_date_to_expiry(values=values, address_as_list=address_as_list, 
+                          purple_list=purple_list, green_list=green_list, red_list=red_list, yellow_list=yellow_list)
+
+
+    else:
+        #a single block was passed in, and handling is simpler
+        values = user_selection.value #low cost
+        address_as_list = block_to_list(address).split(",")
+
+
+        helper_date_to_expiry(values=values, address_as_list=address_as_list, 
+                              purple_list=purple_list, green_list=green_list, red_list=red_list, yellow_list=yellow_list)
+
+
+
+
     color_cells(color_dict={(102,255,102):green_list, (255,102,102): red_list, (255,153,255):purple_list,(255,255,153):yellow_list}, wb=wb)
 
 if __name__ == '__main__':
--- test_gui.py
+++ test_gui.py
@@ -7,3 +7,7 @@ def file_address_gui():
                 [[sg.Text('Filename')], [sg.Input(),sg.FileBrowse()],
                 [sg.OK(), sg.Cancel()]]).read(close=TRUE)
         return values
+
+def simple_error(error_title, error_body):
+    event = sg.Window(error_title,
+                [[sg.Text(error_body), sg.OK()]]).read(close=TRUE)
