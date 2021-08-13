ceead42239ecfc92b95c5b0d5063db97dcdf01c8
EthanTheMathmo <eh595@cam.ac.uk>
2021-08-13 16:40:46 +0100

    error handlin

--- for_mo.py
+++ for_mo.py
@@ -1,6 +1,5 @@
 import xlwings as xw
 from index_helpers import block_to_list
-from formula_colors_speed import color_cells
 from test_gui import file_address_gui
 import datetime
 import re
@@ -56,17 +55,20 @@ def color_cells(color_dict, wb):
     !wb should already be defined in the python file to be the current workbook!
     """
     for color in color_dict:
-        addresses = ",".join(color_dict[color])
-        for addresses_sub_255 in char_lim_255(addresses):
-            wb.sheets.active.range(addresses_sub_255).api.Interior.ColorIndex = color
+        if color_dict[color] != []:
+            addresses = ",".join(color_dict[color])
+            for addresses_sub_255 in char_lim_255(addresses):
+                wb.sheets.active.range(addresses_sub_255).api.Interior.ColorIndex = color
+        else:
+            #if the array is empty, there is nothing to color, so we pass
+            pass
     return
 
 def highlight_dates_to_expiry():
     """
     Given a selection of cells which are dates
-
     TO-DO:
-        ERROR HANDLING FOR IF THE VALUES ARE NOT DATETIMES
+        ERROR HANDLING FOR IF THE VALUES ARE NOT DATETIMES (partially done 13.08.21)
         Change the color coding based on datetimes to ones requested. (Currently: 1 year green, 6mths-1yr yellow, less than 
         6 months is red. Should probably be more of a gradation, perhaps with red set aside for if it has expired?)
     
@@ -74,6 +76,13 @@ def highlight_dates_to_expiry():
     wb = xw.apps.active.books.active #high cost
     user_selection = wb.selection #high cost cost
     values = user_selection.value #low cost
+    if type(values) != list:
+        #e.g., if we select a single cell, this puts it in an array, so that the code runs in the same way 
+        #for multiple values and for single values
+        values = [values]
+    else:
+        pass
+    
     address = user_selection.address #low cost
     address_as_list = block_to_list(address).split(",")
 
@@ -83,6 +92,11 @@ def highlight_dates_to_expiry():
     purple_list = [] #for cells whose values we couldn't read as a date
 
     for i, val in enumerate(values):
+        if val == None:
+            continue
+        else:
+            pass
+        
         if type(val) != datetime.datetime:
             #tries to convert to a datetime if the input formula is a bit off e.g., 18/9/18 instead of 18/9/2018
             a = re.match(r"([0-2]{0,1}[0-9]||30||31)[/.]([0]{0,1}[1-9]||[1][0-2])[/.][0-9][0-9]", val)
@@ -117,7 +131,6 @@ def highlight_dates_to_expiry():
 
     
     color_cells(color_dict={4:green_list, 3: red_list, 13:purple_list,6:yellow_list}, wb=wb)
-
 if __name__ == '__main__':
     # Expects the Excel file next to this source file, adjust accordingly.
     xw.Book(r"C:\Users\ethan\Documents\Mo Forgan demo\Mentor student template.xlsm").set_mock_caller()
